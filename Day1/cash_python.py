ab3bbcd0254c81de65dc76e09daf83d2c6ecd942
Mr.Eldar <elka.bekir@gmail.com>
2023-03-16 21:35:20 -0400

    Day 2 is done

---
+++ Day1/cash_python.py
@@ -0,0 +1,9 @@
+cash = 3485023.3532
+print(cash/8)
+cats2blahblah = 1
+print(cats2blahblah)
+num_of_beds = 99
+num_of_beds = num_of_beds - 1
+print(num_of_beds)
+uncle_Genius2 = 475
+print(uncle_Genius2)
